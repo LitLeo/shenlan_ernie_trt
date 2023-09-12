1b0c6a46656f39df0523b3c0741d74e2eef294dd
leowgyang <leowgyang@tencent.com>
2023-09-12 11:45:39 +0800

    bk

--- scripts/builder.sh
+++ scripts/builder.sh
@@ -1,5 +1,5 @@
 # fp32 build
-python API/builder.py -p ../sti2_data/model/paddle_infer_model -o trt_models/ernie_api_fp32.engine
+python3 API/builder.py -p ../sti2_data/model/paddle_infer_model -o trt_models/ernie_api_fp32.engine
 
 # fp16 build
 #python ./src/builder.py --fp16 -p ./model/paddle_infer_model -o ./model/trt_model/ernie_fp16.engine
