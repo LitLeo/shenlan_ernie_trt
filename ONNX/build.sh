fae2dc6003c5fc1c10eebff69ed46eb0b53ae7e3
leowgyang <leowgyang@tencent.com>
2023-09-20 22:13:11 +0800

    fp16 test done

--- ONNX/build.sh
+++ ONNX/build.sh
@@ -1,1 +1,2 @@
-python onnx2trt.py -m=../../sti2_data/model/onnx_infer_model/model.onnx -o=../trt_models/ernie_onnx_fp32.engine 
+#python ONNX/onnx2trt.py -m=../sti2_data/model/onnx_infer_model/model.onnx -o=trt_models/ernie_onnx_fp32.engine 
+python ONNX/onnx2trt.py -f -m=../sti2_data/model/onnx_infer_model/model.onnx -o=trt_models/ernie_onnx_fp16.engine 
